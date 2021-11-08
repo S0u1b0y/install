798c56d5d572e879616ad35d6278cb1872d9a5b7
SoulBoy <91345838+S0u1b0y@users.noreply.github.com>
2021-11-08 13:35:07 +0300

    Update manjaro_install_xfce.sh

--- manjaro_install_xfce.sh
+++ manjaro_install_xfce.sh
@@ -4,17 +4,17 @@
 # Доп. информация о приложениях Manjaro XFCE - https://gitlab.manjaro.org/profiles-and-settings/iso-profiles/-/blob/master/manjaro/xfce/Packages-Desktop
 
 ## Минимальная установка:
+# xorg-server - Иксы,
 # xfce4 - Минимальная установка XFCE,
+# lxdm - Загрузчик,
 # pavucontrol - Панель управления звуком Pulseaudio,
 # xfce4-xkb-plugin - Плагин раскладки клавиатуры,
 # xfce4-pulseaudio-plugin - Плагин управления звуком,
 # xfce4-clipman-plugin - Плагин расширенного буфера обмена,
 # xfce4-weather-plugin - Плагин погоды,
 # blueman - Поддержка Bluetooth,
-# menulibre - Редактор меню,
-# lxdm - Загрузчик,
-# xorg-server - Иксы.
-sudo pacman --noconfirm -S xfce4 pavucontrol xfce4-xkb-plugin xfce4-pulseaudio-plugin xfce4-clipman-plugin xfce4-weather-plugin menulibre lxdm xorg-server
+# menulibre - Редактор меню.
+sudo pacman --noconfirm -S xorg-server xfce4 lxdm pavucontrol xfce4-xkb-plugin xfce4-pulseaudio-plugin xfce4-clipman-plugin xfce4-weather-plugin menulibre
 sudo systemctl enable lxdm.service
 sudo pacman --noconfirm -Rs xfce4-terminal
 
