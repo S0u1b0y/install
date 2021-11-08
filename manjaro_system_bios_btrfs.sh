188ea5980e20d37e97ebb56f207bd759d8eb8a38
SoulBoy <91345838+S0u1b0y@users.noreply.github.com>
2021-11-08 13:02:38 +0300

    Update manjaro_system_bios_btrfs.sh

--- manjaro_system_bios_btrfs.sh
+++ manjaro_system_bios_btrfs.sh
@@ -123,8 +123,11 @@ systemctl enable sshd.service
 # pipewire - Современный сервер для мультимедийной маршрутизации на замену Pulseaudio, Alsa и Jack,
 # pipewire-alsa, pipewire-pulse, pipewire-jack - Плагины для совместимости с программами написанными под Pulseaudio, Alsa и Jack,
 # gst-plugin-pipewire - Плагин для GStreamer,
+# nvidia, nvidia-utils, lib32-nvidia-utils, nvidia-settings - Драйвера для nVidia,
+# xf86-video-amdgpu, vulkan-radeon, lib32-vulkan-radeon - Драйвера для AMD,
+# xf86-video-intel, vulkan-intel, lib32-vulkan-intel - Драйвера для Intel,
 # xf86-video-vesa, virtualbox-guest-utils - Драйвера для VirtualBox.
-pacman --noconfirm -S pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire xf86-video-vesa virtualbox-guest-utils
+pacman --noconfirm -S pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire nvidia, nvidia-utils, lib32-nvidia-utils, nvidia-settings
 
 ## Установим некоторые утилиты:
 # git - Работа с GitHub и Gitlab,
