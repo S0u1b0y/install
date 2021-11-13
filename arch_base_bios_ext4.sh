a48825ecda4080ad81b3904173af2287aeb835a0
SoulBoy <91345838+S0u1b0y@users.noreply.github.com>
2021-11-13 11:12:56 +0300

    Update arch_base_bios_ext4.sh

--- arch_base_bios_ext4.sh
+++ arch_base_bios_ext4.sh
@@ -37,24 +37,21 @@ Server = http://mirrors.powernet.com.ru/archlinux/$repo/os/$arch
 Server = http://archlinux.zepto.cloud/$repo/os/$arch' | tee /etc/pacman.d/mirrorlist
 pacman --noconfirm -Syy
 
-# Создаем три раздела Root(40Gb-sda1), Home(Остальное-sda2) и EFI(300Mb-sda3):
-parted -s /dev/sda -- mktable gpt \
-    mkpart Root ext4 1M 40G \
-    mkpart Home ext4 40G -300M \
-    mkpart EFI fat32 -300M 100% \
-set 3 esp on
-# Форматируем разделы sda1 и sda2 в ext4, а раздел sda3 в fat32:
+# Создаем два раздела Root(40Gb-sda1) и Home(Остальное-sda2):
+parted -s /dev/sda -- mktable msdos \
+    mkpart primary ext4 1M 43G \
+    mkpart primary ext4 43G 100% \
+    set 1 boot on
+fdisk -l /dev/sda
+# Форматируем разделы в ext4 с метками Root и Home соответственно:
 mkfs.ext4 -L Root /dev/sda1
 mkfs.ext4 -L Home /dev/sda2
-mkfs.fat -F32 /dev/sda3
 # Примонтируем раздел sda1 в /mnt:
 mount /dev/sda1 /mnt
-# Создаем каталоги /mnt/home и /mnt/boot/efi:
-mkdir -p /mnt/{home,boot/efi}
-# Примонтируем раздел sda2 в /mnt/home:
+# Создаем каталог /mnt/home:
+mkdir -p /mnt/home
+# Примонтируем раздел sda2 в /mnt/home,
 mount /dev/sda2 /mnt/home
-# Примонтируем раздел sda3 в /mnt/boot/efi:
-mount /dev/sda3 /mnt/boot/efi
 
 # Ставим систему со стандартным ядром:
 # base, base-devel - Базовая система,
