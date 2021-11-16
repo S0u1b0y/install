2e168db626c771b1e03e2f35f817329ba5b47c9b
SoulBoy <91345838+S0u1b0y@users.noreply.github.com>
2021-11-17 00:43:24 +0300

    Update and rename manjaro_system_bios_btrfs_vbox.sh to manjaro_system_bios_vbox.sh

--- manjaro_system_bios_vbox.sh
+++ manjaro_system_bios_vbox.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
 # Переменные:
-user=user             # Имя пользователя
-hostname=virtual      # Имя компьютера
-netface=enp0s3        # Имя сетевого интерфейса
-ipaddr=192.168.0.50   # IP-адрес компьютера
-gateway=192.168.0.100 # Сетевой шлюз
-swapfile=2048         # Размер файла подкачки (2048Mb)
-
-## Настроим параметры запуска системы на btrfs:
-# Меняем udev на systemd и fsck на keymap.
-sed -i 's/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base systemd autodetect modconf block filesystems keyboard keymap)/' /etc/mkinitcpio.conf
+netface=enp0s3          # Имя сетевого интерфейса (можно узнать командой - ip a)
+ipaddr=192.168.0.50     # Статический IP-адрес компьютера
+gateway=192.168.0.100   # Сетевой шлюз он же роутер
 
 ## Настроим Pacman:
 # Включаем "цветной" режим, раскоментируя параметр "Color",
@@ -19,12 +12,6 @@ sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/g' /etc/pacman.conf
 # Принудительно обновляем репозитории.
 pacman -Syy
-
-## Установим часовой пояс и время:
-# Устанавливаем часовой пояс, в данном случае это Europe/Moscow.
-ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
-# Устанавливаем время по UTC.
-hwclock --systohc --utc
 
 ## Локализуем систему и консоль:
 # Раскоментируем локали en_US и ru_RU в файле locale.gen
@@ -37,22 +24,9 @@ echo -e 'KEYMAP=ru\nFONT=cyr-sun16' > /etc/vconsole.conf
 # И генерируем локали
 locale-gen
 
-## Установим имя компьютера в сети:
-echo "$hostname" > /etc/hostname
-echo "127.0.0.1    localhost" > /etc/hosts
-echo "::1          localhost" >> /etc/hosts
-echo "$ipaddr virtual.localdomain virtual" >> /etc/hosts
-
 ## Введем пароль root:
 echo '>>>> Enter root password <<<<'
 passwd
-
-## Добавляем пользователя и задаем ему пароль:
-# Пропишем пользователя в группы: video,audio,games,lp,optical,power,storage,wheel
-# И установим zsh в качестве командной оболочки по умолчанию (/bin/zsh).
-useradd -m -g users -G video,audio,games,lp,optical,power,storage,wheel -s /bin/zsh $user
-echo ">>>> Enter $user password <<<<"
-passwd $user
 
 ## Установим GRUB:
 # Устанавливаем grub в систему
@@ -61,36 +35,15 @@ pacman --noconfirm -S grub
 grub-install --target=i386-pc /dev/sda
 # Убираем загрузочное меню Grub, меняя GRUB_TIMEOUT с пяти секунд на ноль
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-# Чтобы избежать ошибки ...
-sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/g' /etc/default/grub
-sed -i 's/GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=false/g' /etc/default/grub
+# Что бы небыло ошибки (error: sparse file not allowed):
+#sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/g' /etc/default/grub
+#sed -i 's/GRUB_SAVEDEFAULT=true/GRUB_SAVEDEFAULT=false/g' /etc/default/grub
 # Генерируем файл конфигурации grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 ## Настроим sudo:
 # Убираем коментарий с группы %wheel.
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
-
-## Создадим файл подкачки (swapfile):
-# Создаем подтом Btrfs: @swap
-btrfs subvolume create /@swap
-# Переходим в @swap
-cd /@swap
-# Создаём пустой файл подкачки
-truncate -s 0 ./swapfile
-# Отключаем копирование при записи для файла подкачки
-chattr +C ./swapfile
-# Отключаем сжатие файла подкачки
-btrfs property set ./swapfile compression none
-# Создаём файл нужного размера
-dd if=/dev/zero of=./swapfile bs=1M count=$swapfile status=progress
-# Разрешаем доступ к файлу подкачки только root-у
-chmod 600 ./swapfile
-# Инициализируем файл подкачки и включаем его
-mkswap ./swapfile
-swapon ./swapfile
-# Прописываем в fstab, автомонтирование файла подкачки при загрузке системы
-echo -e '# Swapfile\n/@swap/swapfile none swap sw 0 0' >> /etc/fstab
 
 ## Установим и настроим Zswap:
 # Устанавливаем
@@ -104,14 +57,14 @@ sed -i 's/RAM_SIZE \/ 4/RAM_SIZE \/ 2/' /etc/systemd/swap.conf
 # Включаем загрузку при старте системы
 systemctl enable systemd-swap.service
 
-## Настроим сеть (StaticIP - 192.168.0.50, Gateway - 192.168.0.100):
+## Настроим сеть (StaticIP):
 # Устанавливаем демон (службу) dhcpcd
 pacman --noconfirm -S dhcpcd
 # Включаем загрузку при старте системы
 systemctl enable dhcpcd.service
 # Делаем резервную копию файла настроек, на всякий случай
 cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-# Прописываем в файл конфигурации параметры настройки сети
+# Прописываем, в файл конфигурации, параметры настройки сети
 echo -e "interface $netface" > /etc/dhcpcd.conf
 echo -e "static ip_address=$ipaddr/24" >> /etc/dhcpcd.conf
 echo -e "static routers=$gateway" >> /etc/dhcpcd.conf
@@ -135,14 +88,6 @@ systemctl enable sshd.service
 # gst-plugin-pipewire - Плагин для GStreamer,
 pacman --noconfirm -S pipewire pipewire-alsa pipewire-pulse pipewire-jack gst-plugin-pipewire
 
-## Установим драйвера на видео:
-# nvidia nvidia-utils lib32-nvidia-utils nvidia-settings - Драйвера для nVidia,
-# gwe - утилита для разгона и мониторинга видеокарт nVidia (опционально),
-# xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon - Драйвера для AMD,
-# xf86-video-intel vulkan-intel lib32-vulkan-intel - Драйвера для Intel.
-# xf86-video-vesa, virtualbox-guest-utils - Драйвера для VirtualBox.
-pacman --noconfirm -S xf86-video-vesa virtualbox-guest-utils
-
 ## Установим архиваторы и поддержку некоторых сетевых протоколов и ФС:
 # p7zip unrar unace lrzip - Работа с архивами,
 # cifs-utils - Поддержка подключения к Samba,
@@ -155,5 +100,9 @@ pacman --noconfirm -S p7zip unrar unace lrzip cifs-utils davfs2 gvfs gvfs-smb gv
 # curl wget - Консольные загрузчики,
 # mc - Консольный файловый менеджер Midnight Comander,
 # htop - Мониторинг параметров системы из консоли,
-# neofetch - Информация о системе в консоли.
+# neofetch - Информация о системе в консоли,
+# Менеджер aur-пакетов - yay.
 pacman --noconfirm -S git curl wget mc htop neofetch yay
+
+# Шрифты:
+sudo pacman --noconfirm -S ttf-ubuntu-font-family ttf-liberation ttf-dejavu ttf-droid ttf-hack ttf-roboto ttf-roboto-mono noto-fonts
